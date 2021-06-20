4ad1fb83096a3738b6c70f476c9ec12f509511fd
Hakan Tunc <dev@hakantunc.com>
2021-06-19 21:33:44 -0700

    Display response in a table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+type Data = {
+  name: string;
+  age: number;
+};
+
+type ActionResponse = {
+  data: Data[];
+};
 
 function App() {
+  const [state, setState] = React.useState<ActionResponse>({ data: [] });
+
+  React.useEffect(() => {
+    fetch("http://localhost:5000/action")
+      .then((response) => response.json())
+      .then((data) => {
+        setState(data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+
+  const tableRows = state.data.map(({ name, age }) => {
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>{age}</td>
+      </tr>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello there</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </table>
     </div>
   );
 }
